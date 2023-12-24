569ba98d04221140e8f89405243b172ed8176348
Raji, Amine <amine.raji@volvocars.com>
2023-12-24 18:22:42 +0100

    ExternalDNS deployed with TF

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -15,6 +15,13 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.this.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+}
+
 # External nginx
 resource "helm_release" "external_nginx" {
   name = "external"
@@ -45,14 +52,14 @@ resource "helm_release" "cert_manager" {
 }
 
 # # External DNS
-# resource "helm_release" "external_dns" {
-#   name = "external-dns"
+resource "helm_release" "external_dns" {
+  name = "external-dns"
 
-#   repository       = "https://charts.bitnami.com/bitnami"
-#   chart            = "external-dns"
-#   namespace        = "external-dns"
-#   create_namespace = true
-#   version          = "6.28.6"
+  repository       = "https://charts.bitnami.com/bitnami"
+  chart            = "external-dns"
+  namespace        = "external-dns"
+  create_namespace = true
+  version          = "6.28.6"
 
-#   values = [file("${path.module}/values/external-dns.yaml")]
-# }
+  values     = [file("${path.module}/values/external-dns.yaml")]
+}
