a68ab119576956d2dd9482f047c81b011954a96b
Raji, Amine <amine.raji@volvocars.com>
2023-12-24 08:10:08 +0100

    Removeing unecessary ssh file + variables

--- variables.tf
+++ variables.tf
@@ -15,25 +15,25 @@
 #   default = "my-aks-cluster"
 # }
 
-variable "kubernetes_rbac_admin_groups" {
-  type    = string
-  default = "kubernetes-rbac-admin-groups"
-}
+# variable "kubernetes_rbac_admin_groups" {
+#   type    = string
+#   default = "kubernetes-rbac-admin-groups"
+# }
 
-variable "node_count" {
-  type        = number
-  description = "The initial quantity of nodes for the node pool."
-  default     = 2
-}
+# variable "node_count" {
+#   type        = number
+#   description = "The initial quantity of nodes for the node pool."
+#   default     = 2
+# }
 
-variable "msi_id" {
-  type        = string
-  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
-  default     = null
-}
+# variable "msi_id" {
+#   type        = string
+#   description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
+#   default     = null
+# }
 
-variable "username" {
-  type        = string
-  description = "The admin username for the new cluster."
-  default     = "azureadmin"
-}
+# variable "username" {
+#   type        = string
+#   description = "The admin username for the new cluster."
+#   default     = "azureadmin"
+# }
